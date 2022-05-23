b28449c1f1ac912ffcbf8333b520f24351481c8e
Federico Carbone <carbone.federico@gmail.com>
2022-05-23 14:53:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sample PingOne for Customers Spring Boot Applications
+Sample Java Spring Boot OIDC integration, used to test and demonstrate OIDC SSO flows with PingFederate
+
+
+
+
---
+++ src/main/java/com/pingidentity/oidclogin/IndexController.java
@@ -0,0 +1,29 @@
+package com.pingidentity.oidclogin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for the index page.
+ */
+@Controller
+public class IndexController {
+	
+	Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+    @GetMapping("/")
+    public String index (Model model, @AuthenticationPrincipal OidcUser principal) {
+    	logger.debug("Starting index with user " + principal);
+        if (principal != null) {
+            model.addAttribute("profile", principal.getClaims());
+       
+        }
+        return "index";
+    }
+}
